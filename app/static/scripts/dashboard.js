f946691abbafd565e2173d72e5f4d2350c4d26e1
Jay0x13 <dilucrag19@gmail.com>
2024-05-16 16:42:35 +0800

    update

--- app/static/scripts/dashboard.js
+++ app/static/scripts/dashboard.js
@@ -1,9 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
+
   const menu = document.querySelector('.toggle-menu');
   menu.addEventListener('click', () => {
     const body = document.querySelector('.body');
     body.classList.toggle('active');
+
+    projectDropDown();
   });
+
+  function projectDropDown() {
+    const body = document.querySelector('.body');
+    const toggler = document.querySelector('.nav-btn.navbar-toggler');
+    const dropdown = document.querySelector('#projectFilter');
+    if (!body.classList.contains('active')) {
+      toggler.removeAttribute('data-bs-target');
+      dropdown.classList.remove('show');
+    } else {
+      if (!toggler.getAttribute('data-bs-target')) {
+        toggler.setAttribute('data-bs-target', '#projectFilter');
+      }
+    }
+  }
 
   function handleClicks(event) {
     const navBtn = event.target.closest('.nav-bar .nav-btn');
@@ -25,4 +42,5 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   document.addEventListener('click', handleClicks);
+  projectDropDown();
 });
