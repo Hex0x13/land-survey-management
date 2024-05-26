03c17e9489bd949632250a904776c10df495ee6f
Jay0x13 <dilucrag19@gmail.com>
2024-05-26 14:31:29 +0800

    update

--- app/views.py
+++ app/views.py
@@ -1,13 +1,6 @@
 from flask import render_template
 from app import app
 
-@app.route('/sign-in', methods=['GET'])
-def sign_in():
-  return render_template('sign-in.html')
-
-@app.route('/login', methods=['GET'])
-def login():
-  return render_template('login.html')
 
 @app.route('/', methods=['GET'])
 def dashboard():
